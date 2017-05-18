53565f0d0e48abf4d0b09bcd8b45650e8553e586
15035634 <15035634@myrp.edu.sg>
2017-05-18 15:45:00 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/databasecrud/Note.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.c347.databasecrud;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15035634 on 18/5/2017.
+ */
+
+public class Note implements Serializable {
+
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() { return id; }
+
+    public String getNoteContent() { return noteContent; }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+
+}
